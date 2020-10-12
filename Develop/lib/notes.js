68a786893dd50642742dabe4dfc500d8f84b0a27
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-12 00:22:02 -0500

    notes.js

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -1,2 +1,36 @@
 const path = require('path');
 const fs = require('fs');
+  
+  function findById(id, noteArray) {
+    const result = noteArray.filter(note => note.id === id)[0];
+    return result;
+  }
+  
+  function createNewNote(body, noteArray) {
+    const note = body;
+    noteArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, '../db/db.json'),
+      JSON.stringify({notes: noteArray}, null, 2)
+    );
+    return note;
+  }
+  
+  function validateNote(note) {
+    if (!note.name || typeof note.name !== 'string') {
+      return false;
+    }
+    if (!note.content || typeof note.content !== 'string') {
+      return false;
+    }
+    if (!note.id || typeof note.id !== 'string') {
+      return false;
+    }
+    return true;
+  }
+
+    module.exports = {
+        findById,
+        createNewNote,
+        validateNote
+      };
--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 app.use(express.static('public'));
 
   // HTML Routes
