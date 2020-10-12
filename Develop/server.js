aa2d5fb4983bb7b9730023846881427e0c4af9e5
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-10-11 21:27:11 -0500

    added app.listen, added 2 get routes to connect index and notes htmls

--- Develop/server.js
+++ Develop/server.js
@@ -1,2 +1,24 @@
+// Dependencies
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
 const fs = require('fs');
+const path = require('path');
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+  // HTML Route
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+  app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
